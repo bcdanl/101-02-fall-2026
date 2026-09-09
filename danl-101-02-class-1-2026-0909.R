d5235aa0b372139adbd2c4ce7ae1553998482599
bcdanl <bchoe@geneseo.edu>
2026-09-09 11:51:55 -0400

    0909

---
+++ danl-101-02-class-1-2026-0909.R
@@ -0,0 +1,36 @@
+a <- 1
+b <- 2
+
+
+install.packages("tidyverse")
+
+# install.packages("readr")
+# install.packages("dplyr")
+# install.packages("ggplot2")
+#  ......
+#  ......
+#  ......
+#  ......
+#  ......
+
+# Below installs the collection of R packages, tidyverse
+# install.packages("tidyverse")
+# 
+
+library(tidyverse)
+mpg
+
+df_mpg <- mpg
+
+ggplot2::mpg
+
+
+df <- read_csv("https://bcdanl.github.io/data/spotify_all.csv")
+
+
+
+
+
+
+
+
---
+++ danl-101-02-class-2-2026-0909.R
@@ -0,0 +1,14 @@
+# Below installs the collection of R packages, tidyverse
+# install.packages("tidyverse")
+# 
+
+library(tidyverse)  # Load tidyverse package
+mpg
+
+df_mpg <- mpg
+
+ggplot2::mpg
+
+
+df <- read_csv("https://bcdanl.github.io/data/spotify_all.csv")
+
